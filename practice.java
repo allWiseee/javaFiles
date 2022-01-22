70a8485f8a27f81686c5c04b79febc5b778db95f
tdunmore1 <tdunmore1@gmail.com>
2022-01-22 14:08:32 -0500

    redo changes

--- practice.java
+++ practice.java
@@ -10,6 +10,7 @@ class practice {
         double average = (maths + english + science + drama)/tests;
         //Print statement
         System.out.println(average);
+        System.out.println("good to go");
     }
   }
 
